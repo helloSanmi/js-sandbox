6cfd841ff115f343bc9419995019b86ff7b8f80e
Oluwasanmi Idowu <95680393+helloSanmi@users.noreply.github.com>
2022-04-01 20:55:26 +0100

    Update js-calculator.js

--- js-calculator.js
+++ js-calculator.js
@@ -1,9 +1,7 @@
 //a simple calculator
 
 //declare global variables
-let firstNum, secondNum, result;
-let userInput;
-let operation;
+let firstNum, secondNum, result, userInput, operation;
 
 //prompt user to select operation
 userInput = parseInt(prompt('Welcome, which operation will you like to perform:\n Press 1 for Addition \n Press 2 for Subtraction \n Press 3 for Multiplication \n Press 4 for Division'))
