5523f0a64c4d3efc22710ee9acba3313a8c7366f
Oluwasanmi Idowu <95680393+helloSanmi@users.noreply.github.com>
2022-04-01 15:44:29 +0100

    Add files via upload

---
+++ js-calculator.js
@@ -0,0 +1,37 @@
+//a simple calculator
+
+//declare global variables
+let firstNum, secondNum, result;
+
+//prompt user to select operation
+userInput = parseInt(prompt('Welcome, which operation will you like to perform:\n Press 1 for Addition \n Press 2 for Subtraction \n Press 3 for Multiplication \n Press 4 for Division'))
+
+//prompt and store the numbers
+firstNum = parseFloat(prompt('Enter the first number'));
+secondNum = parseFloat(prompt('Enter the second number'));
+
+//conditional statement for the operations
+if (userInput === 1){
+    result = firstNum + secondNum
+    operation = 'Addition'
+}
+else if (userInput === 2){
+    result = firstNum - secondNum
+    operation = 'Subtraction'
+}
+else if (userInput === 3){
+    result = firstNum * secondNum
+    operation = 'Multiplication'
+}
+else if (userInput === 4){
+    result = firstNum / secondNum
+    operation = 'Division'
+}
+else if(userInput > 4){
+    alert('Invalid operation')
+}
+else {
+    alert('Invalid selection')
+}
+
+alert('Result for '+operation+ ': ' +result)
