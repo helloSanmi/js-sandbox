8570dbf80c6919826a10a6e24e6588f943d45e39
Oluwasanmi Idowu <95680393+helloSanmi@users.noreply.github.com>
2022-04-04 13:59:13 +0100

    Add files via upload

--- js-calculator.js
+++ js-calculator.js
@@ -1,7 +1,25 @@
 //a simple calculator
 
+//operation functions
+function add(a, b){
+    return a + b;
+}
+
+function subt(a, b){
+    return a - b;
+}
+
+function multy(a, b){
+    return a * b;
+}
+
+function divs(a, b){
+    return a / b;
+}
+
+
 //declare global variables
-let firstNum, secondNum, result, userInput, operation;
+let firstNum, secondNum, result, operation, userInput;
 
 //prompt user to select operation
 userInput = parseInt(prompt('Welcome, which operation will you like to perform:\n Press 1 for Addition \n Press 2 for Subtraction \n Press 3 for Multiplication \n Press 4 for Division'))
@@ -12,27 +30,29 @@ secondNum = parseFloat(prompt('Enter the second number'));
 
 //conditional statement for the operations
 if (userInput === 1){
-    result = firstNum + secondNum
-    operation = 'Addition'
-}
+        result = add(firstNum, secondNum)
+        operation = 'Addition'
+    }
 else if (userInput === 2){
-    result = firstNum - secondNum
-    operation = 'Subtraction'
-}
+        result = subt(firstNum, secondNum)
+        operation = 'Subtraction'
+    }
 else if (userInput === 3){
-    result = firstNum * secondNum
-    operation = 'Multiplication'
-}
+        result = multy(firstNum, secondNum)
+        operation = 'Multiplication'
+    }
 else if (userInput === 4){
-    result = firstNum / secondNum
-    operation = 'Division'
-}
+        result = divs(firstNum, secondNum)
+        operation = 'Division'
+    }
 else if(userInput > 4){
-    alert('Invalid operation')
-}
+        alert('Invalid operation')
+    }
 else {
-    alert('Invalid selection')
-}
+        alert('Invalid selection')
+    }
 
-//display result
+//final result
 alert('Result for '+operation+ ': ' +result)
+
+
