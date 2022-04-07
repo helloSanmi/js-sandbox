e578e400320c325c4d182d08538452262f8e12aa
Oluwasanmi Idowu <95680393+helloSanmi@users.noreply.github.com>
2022-04-07 20:57:53 +0100

    Update array.js

--- array.js
+++ array.js
@@ -8,15 +8,19 @@ console.log(array)
 
 //sort the array in order
 array.sort()
+console.log(array)
 
 //put 'Kiwi' at the end of the array
 array.push('Kiwi')
+console.log(array)
 
 //remove 'Apples' from the array
 array.shift()
+console.log(array)
 
 //sort the array in reverse order
 array.reverse()
+console.log(array)
 
 //second array
 var array2 = ["Banana", ["Apples", ["Oranges"], "Blueberries"]];
