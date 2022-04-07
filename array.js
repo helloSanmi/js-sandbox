8573e0d656890f7bb34a41f969863869b08e7e79
Oluwasanmi Idowu <95680393+helloSanmi@users.noreply.github.com>
2022-04-07 11:04:00 +0100

    Create array.js

---
+++ array.js
@@ -0,0 +1,22 @@
+var array = ['Banana', 'Apples', 'Oranges', 'Blueberries'];
+
+console.log(array)
+// //remove the banana from the array
+array.shift()
+console.log(array)
+
+//diplay the shifted element
+// let eShift = array.shift()
+// console.log(eShift)
+
+// //sort the array in order
+array.sort()
+
+//put 'Kiwi' at the end of the array
+array.push('Kiwi')
+
+//remove 'Apples' from the array
+delete array[0];
+
+//sort the array in reverse order
+array.reverse()
