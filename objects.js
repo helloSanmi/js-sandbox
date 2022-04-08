ab2eb09a0e5200c8375f637d47dc138299bbd156
Oluwasanmi Idowu <95680393+helloSanmi@users.noreply.github.com>
2022-04-08 13:49:51 +0100

    Create objects.js

---
+++ objects.js
@@ -0,0 +1,25 @@
+//object with username and password properties
+var user = {
+  username: "ajayi",
+  password: "goforit"
+};
+
+//array that contains the object user
+var database = [user];
+
+//array that contains 3 objects with properties username and password
+var newsfeed = [
+  {
+    username: "marvelous",
+    timeline: ""
+  },
+  {
+    username: "buju",
+    timeline: ""
+  },
+  {
+    username: "olamide",
+    timeline: ""
+  }
+];
+
